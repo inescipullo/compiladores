ab8bec490f9cc9b3e1fd41d15cb423ad49381b94
Katherine Sullivan <katherine@fceia.unr.edu.ar>
2024-09-10 19:31:21 -0300

    idea de elaboracion

--- src/Elab.hs
+++ src/Elab.hs
@@ -14,6 +14,7 @@ module Elab ( elab, elabDecl) where
 
 import Lang
 import Subst
+import MonadFD4
 
 -- | 'elab' transforma variables ligadas en índices de de Bruijn
 -- en un término dado. 
@@ -31,6 +32,9 @@ elab' env (SV p v) =
 elab' _ (SConst p c) = Const p c
 elab' env (SLam p (v,ty) t) = Lam p v ty (close v (elab' (v:env) t))
 elab' env (SFix p (f,fty) (x,xty) t) = Fix p f fty x xty (close2 f x (elab' (x:f:env) t))
+-- aca los tipos con listas de binders los voy a traer en listas unarias, si no fallo con monadfd4
+-- tambien lo mismo con las listas de vars
+
 elab' env (SIfZ p c t e)         = IfZ p (elab' env c) (elab' env t) (elab' env e)
 -- Operadores binarios
 elab' env (SBinaryOp i o t u) = BinaryOp i o (elab' env t) (elab' env u)
@@ -40,6 +44,35 @@ elab' env (SPrint i str t) = Print i str (elab' env t)
 elab' env (SApp p h a) = App p (elab' env h) (elab' env a)
 elab' env (SLet p (v,vty) def body) =  
   Let p v vty (elab' env def) (close v (elab' (v:env) body))
+
+-- | desazucareo lo nuevo
+
+-- | convierto multibinders en binders simples
+demultibinding :: [([Name],Ty)] -> [([Name],Ty)]
+demultibinding [] = []
+demultibinding ((vs,ty):bs) = map (\v -> ([v],ty)) vs ++ demultibinding bs
+
+-- | si tengo argumentos es una fun
+let2fun :: STerm -> STerm
+let2fun (SLet p b bs def body) = SLam p (head bs) (SLet p b (tail bs) def body)
+-- revisar bastante
+-- va a ser recursiva
+let2fun x = x
+
+-- | resticciones en cant de binders 
+-- | sfix necesita dos args
+-- | slet necesita 1 arg
+checkCantArgs :: MonadFD4 m => STerm -> m STerm
+checkCantArgs fix@(SFix p (x:y:xs) t) = return fix
+checkCantArgs (SFix p _ _) = failPosFD4 p "Fix necesita al menos dos argumentos"
+checkCantArgs (SLet p b [] def body) = failPosFD4 p "Let necesita al menos un argumento"
+checkCantArgs (SLet p True [x] def body) = failPosFD4 p "Let rec necesita al menos dos argumentos"
+checkCantArgs x = return x
+
+-- | eta expandir sprintun
+printun2fun :: STerm -> STerm
+printun2fun (SPrintUn p str) = SLam p [([x],Nat)] (SPrint p str (SV p x))
+printun2fun x = x 
 
 elabDecl :: Decl STerm -> Decl Term
 elabDecl = fmap elab
